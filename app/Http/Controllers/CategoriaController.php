0f9cd0d5a1da63dcaf27059e74104cf8cea9f717
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-21 17:44:49 +0200

    Clase 299

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(['categorias' => Categoria::all()]);
+    }
 }
