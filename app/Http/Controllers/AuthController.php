bc2575e0f7bfe3e054a2b1269d7185e1ef874b38
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-22 19:12:19 +0200

    Clase 308

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+    }
+    public function login(Request $request)
+    {
+
+    }
+    
+    public function logout(Request $request)
+    {
+
+    }
+}
