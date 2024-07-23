f0f9d9237acc9f3d9046131edb12c3658e5b5f04
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-23 18:33:35 +0200

    Clase 316

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\RegistroRequest;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -12,6 +13,17 @@ class AuthController extends Controller
     {
         //Validando el registro
         $data = $request->validated();
+        //Creando el usuario
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+        //Retornando una respuesta
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
     }
     public function login(Request $request)
     {
