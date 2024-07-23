64a14bcbac8302a7898e279eb194fa3f44f6c0b1
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-23 12:04:34 +0200

    Clase 310

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,18 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\RegistroRequest;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegistroRequest $request)
     {
-
+        //Validando el registro
+        $data = $request->validated();
     }
     public function login(Request $request)
     {
 
     }
-    
+
     public function logout(Request $request)
     {
 
