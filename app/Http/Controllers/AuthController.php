8af7e9ffe9a6a806c5a582df47dda96793dc162b
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-23 19:33:42 +0200

    Clase 317

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use App\Http\Requests\RegistroRequest;
 use App\Models\User;
@@ -25,9 +26,10 @@ class AuthController extends Controller
             'user' => $user
         ];
     }
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
         //return "Ya estamos dentro del login.";
+        $data = $request->validated();
     }
 
     public function logout(Request $request)
