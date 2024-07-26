d4ae23e7bec793658edfa38a4fa41976de356b52
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-26 14:24:43 +0200

    Clase 324

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,16 @@ use App\Http\Controllers\ProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/* Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum'); */
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/user', function(Request $request){
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 /* Route::get('/categorias',[CategoriaController::class,'index']); */
 Route::apiResource('/categorias', CategoriaController::class);
