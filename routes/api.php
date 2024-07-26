970740e44087e2a0d90cb955e436b4680d67aa4d
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-26 19:09:53 +0200

    Clase 326

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\PedidoController;
 use App\Http\Controllers\ProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,6 +16,9 @@ Route::middleware('auth:sanctum')->group(function() {
         return $request->user();
     });
     Route::post('/logout', [AuthController::class, 'logout']);
+    
+    //Almacenamiento de órdenes
+    Route::apiResource('/pedidos', PedidoController::class);
 });
 
 /* Route::get('/categorias',[CategoriaController::class,'index']); */
