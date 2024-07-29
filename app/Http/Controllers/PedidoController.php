2f874e38b62bb0c7f94a3a7ce3ccfe4303441dde
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-07-29 11:17:34 +0200

    Clase 330

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Pedido;
 use App\Http\Controllers\Controller;
+use App\Models\PedidoProducto;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -28,8 +30,26 @@ class PedidoController extends Controller
         $pedido->total = $request->total;
         $pedido->save();
         
+        //Obtener id del pedido que vamos a guardar
+        $id = $pedido->id;
+        //Obtener los productos
+        $productos = $request->productos;
+        //Formatear Array con id's y cantidades
+        $pedido_producto = [];
+        foreach ($productos as $producto)
+        {
+            $pedido_producto[] = [
+                'pedido_id' => $id,
+                'producto_id' => $producto['id'],
+                'cantidad' => $producto['cantidad'],
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ];
+        }
+        //Almacenar en la BD
+        PedidoProducto::insert($pedido_producto);
         return [
-            'message' => 'Realizando pedido',
+            'message' => 'Realizando pedido '
         ];
     }
 
